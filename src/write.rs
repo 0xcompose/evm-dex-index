b4420893afab3b8c9f39d393239ced6c55fb8491
slava <v.vladykin@ci-id.net>
2026-01-11 23:20:01 +0300

    refactor(write): update write function to use ProtocolDeployments struct and improve JSON serialization

--- src/write.rs
+++ src/write.rs
@@ -1,35 +1,30 @@
 use std::{
-    collections::HashMap,
     fs::{self, File},
     io::BufWriter,
     path::Path,
 };
 
-use crate::types::ProtocolDeployment;
+use crate::types::ProtocolDeployments;
 
 pub fn write(
     folder: &str,
-    protocol_deployments: HashMap<u64, ProtocolDeployment>,
+    protocol_deployments: ProtocolDeployments,
 ) -> Result<(), std::io::Error> {
     if !Path::new(folder).exists() {
         fs::create_dir_all(folder)?;
     }
 
-    let protocol_path = format!(
-        "{}/{}",
-        folder,
-        protocol_deployments.values().next().unwrap().name
-    );
+    let protocol_path = format!("{}/{}", folder, protocol_deployments.protocol_name);
 
     if !Path::new(&protocol_path).exists() {
         fs::create_dir_all(&protocol_path)?;
     }
 
-    for (chain_id, protocol_deployment) in protocol_deployments {
+    for (chain_id, contracts) in protocol_deployments.chains {
         let path = format!("{}/{}.json", protocol_path, chain_id);
         let file = File::create(path)?;
         let mut writer = BufWriter::new(file);
-        serde_json::to_writer_pretty(&mut writer, &protocol_deployment)?;
+        serde_json::to_writer_pretty(&mut writer, &contracts)?;
     }
 
     Ok(())
