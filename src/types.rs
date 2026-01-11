8afe9f56c4928711dcd02d87e02f4c40b3638a4c
slava <v.vladykin@ci-id.net>
2026-01-11 23:14:49 +0300

    feat(types): add ProtocolDeployments struct and update ProtocolDeployment fields

--- src/types.rs
+++ src/types.rs
@@ -3,10 +3,26 @@ use std::collections::HashMap;
 use serde::Serialize;
 
 #[derive(Debug, Serialize)]
+pub struct ProtocolDeployments {
+    pub protocol_name: String,
+    pub chains: ChainDeployments,
+}
+
+pub type ChainId = u64;
+
+pub type ChainDeployments = HashMap<ChainId, ChainContracts>;
+
+pub type ChainContracts = HashMap<ContractName, ContractAddress>;
+
+pub type ContractName = String;
+
+pub type ContractAddress = String;
+
+#[derive(Debug, Serialize)]
 pub struct ProtocolDeployment {
-    name: String,
-    chain_id: u64,
-    contracts: HashMap<String, String>,
+    pub name: String,
+    pub chain_id: ChainId,
+    pub contracts: HashMap<String, String>,
 }
 
 impl ProtocolDeployment {
